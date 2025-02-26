c98efa0df371e76ea7b268bd82938a5b6afb02d4
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2025-02-26 22:02:22 +0200

    add

---
+++ src/js/render-functions.js
@@ -0,0 +1,4 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// gallary створи функції для відображення елементів інтерфейсу (додавання/оновлення елементів галереї, відображення лоедера
